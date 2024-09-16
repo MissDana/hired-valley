0960a46a4a131db8c7f9ba3f8bbc9bb10baf5d21
MissDana <130344484+MissDana@users.noreply.github.com>
2024-09-16 11:31:14 +0300

    Add files via upload

---
+++ src/components/Stats/BarChart.jsx
@@ -0,0 +1,102 @@
+import Chart from "react-apexcharts";
+
+const BarChart = ({darkMode}) => {
+    const chartConfig = {
+        series: [
+    {
+      name: "Sales",
+      data: [40, 60, 80, 100, 150, 300, 350, 500, 350, 200, 230, 500],
+    },
+  ],
+  options: {
+    chart: {
+      type: "bar",
+      height: 240,
+      toolbar: {
+        show: false,
+      },
+    },
+    title: {
+      show: false,
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    colors: ["#020617"],
+    plotOptions: {
+      bar: {
+        columnWidth: "40%",
+        borderRadius: 2,
+      },
+    },
+    xaxis: {
+      axisTicks: {
+        show: false,
+      },
+      axisBorder: {
+        show: false,
+      },
+      labels: {
+        style: {
+          colors: darkMode ? "#dddddd" : "#616161",
+          fontSize: "12px",
+          fontFamily: "inherit",
+          fontWeight: 400,
+        },
+      },
+      categories: [
+        "January",
+        "February",
+        "March",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+    },
+    yaxis: {
+      labels: {
+        style: {
+          colors: darkMode ? "#dddddd" : "#616161",
+          fontSize: "12px",
+          fontFamily: "inherit",
+          fontWeight: 400,
+        },
+      },
+    },
+    grid: {
+      show: true,
+      borderColor: "#a0a0a0",
+      strokeDashArray: 5,
+      xaxis: {
+        lines: {
+          show: true,
+        },
+      },
+      padding: {
+        top: 5,
+        right: 20,
+      },
+    },
+    fill: {
+      opacity: 0.8,
+    },
+    tooltip: {
+      theme: "dark",
+    },
+  },
+};
+
+  return (
+    <div className="px-2 pb-0">
+      <Chart options={chartConfig.options} series={chartConfig.series} type="bar" height={240}/>
+    </div>
+  );
+};
+
+export default BarChart;
